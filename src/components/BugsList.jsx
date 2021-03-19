d4499b14d12c4058901f897937fec725a924a62e
s-satsangi <s.satsangi@gmail.com>
2021-03-19 01:44:42 -0400

    added resolve bug button

--- src/components/BugsList.jsx
+++ src/components/BugsList.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { loadBugs, getUnresolvedBugs } from "../store/bugs";
+import { loadBugs, getUnresolvedBugs, resolveBug } from "../store/bugs";
 
 const BugsList = () => {
   const dispatch = useDispatch();
   const bugs = useSelector(getUnresolvedBugs);
+
   useEffect(() => {
     dispatch(loadBugs());
   }, []);
@@ -12,7 +13,10 @@ const BugsList = () => {
   return (
     <ul>
       {bugs.map((bug) => (
-        <li key={bug.id}>{bug.description}</li>
+        <li key={bug.id}>
+          {bug.description}
+          <button onClick={() => dispatch(resolveBug(bug.id))}>resolve!</button>
+        </li>
       ))}
     </ul>
   );
