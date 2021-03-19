37eb140e8d8ad7d5b9cd314bd4d5e1b4940be278
s-satsangi <s.satsangi@gmail.com>
2021-03-19 00:29:30 -0400

    set up store with functional stateless components and hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Bugs from "./components/Bugs";
 import configureStore from "./store/configureStore";
 import { Provider } from "react-redux";
+import BugsList from "./components/BugsList";
 
 const store = configureStore();
 
 function App() {
   return (
     <Provider store={store}>
-      <Bugs />
+      <BugsList />
     </Provider>
   );
 }
